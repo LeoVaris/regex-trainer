5b0dd1042c59dca6498a738492a99d363bc7ff5b
LeoVaris <leovaris04@gmail.com>
2021-01-03 16:58:12 +0200

    improved styles

--- example_data.sql
+++ example_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO tasks (name, task_info, type) VALUES ('Ohje 1', 'Komento <code>+</code> matchaa kaikki merkit', 2);
 INSERT INTO tasks (name, task_info, type) VALUES ('Tehtävä 1', 'Toteuta lauseke, joka hyväksyy kaikki merkkijonot, jotka sisältävät ainoastaan merkkiä <code>a</code>', 1);
 
-INSERT INTO tests (task_id, data, accept) VALUES (1, 'aaaa', TRUE);
-INSERT INTO tests (task_id, data, accept) VALUES (1, 'aa', TRUE);
-INSERT INTO tests (task_id, data, accept) VALUES (1, 'ab', FALSE);
-INSERT INTO tests (task_id, data, accept) VALUES (1, 'b', FALSE);
-INSERT INTO tests (task_id, data, accept) VALUES (1, 'c', FALSE);
+INSERT INTO tests (task_id, data, accept) VALUES (2, 'aaaa', TRUE);
+INSERT INTO tests (task_id, data, accept) VALUES (2, 'aa', TRUE);
+INSERT INTO tests (task_id, data, accept) VALUES (2, 'ab', FALSE);
+INSERT INTO tests (task_id, data, accept) VALUES (2, 'b', FALSE);
+INSERT INTO tests (task_id, data, accept) VALUES (2, 'c', FALSE);
